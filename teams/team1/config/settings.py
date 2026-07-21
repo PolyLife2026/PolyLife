8415b65d55758dc8cd20c6ebe1fe4581cd1a0186
Pendar <pendar.ra@gmail.com>
2026-07-21 23:31:56 +0330

    add: configure REST framework pagination settings

--- teams/team1/config/settings.py
+++ teams/team1/config/settings.py
@@ -57,3 +57,9 @@ DATABASES = {
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, # Number of items per page
+}
