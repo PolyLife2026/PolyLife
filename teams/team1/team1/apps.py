750f9893184d8398d22ac59a8d3c730c0493316a
Alireza <alireza.sa.2005@gmail.com>
2026-07-23 12:09:17 +0330

    [team1] fix: leaderboard and calculate participant score

--- teams/team1/team1/apps.py
+++ teams/team1/team1/apps.py
@@ -2,7 +2,8 @@ from django.apps import AppConfig
 
 
 class Team1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'team1'
-def ready(self):
-    import team1.signals
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "team1"
+
+    def ready(self):
+        import team1.signals
